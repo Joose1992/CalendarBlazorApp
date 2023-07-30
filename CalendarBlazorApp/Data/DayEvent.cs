c46779aeee980cf33d390488b4c6df9e704600a2
jose montanez <josemontanez1992@gmail.com>
2023-07-30 10:45:13 -0600

    Created dayevent class modify calendar header

---
+++ CalendarBlazorApp/Data/DayEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace CalendarBlazorApp.Data
+{
+    public class DayEvent
+    {
+        public int DayEventID { get; set; }
+        public string Note { get; set; }
+        public DateTime DateEvent { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime FromDate { get; set; } = new DateTime(1900, 1, 1);
+        public DateTime ToDate { get; set; } = new DateTime(1900, 1, 1);
+        public string DateValue { get; set; }
+        public string DayName { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ CalendarBlazorApp/Data/WeekClass.cs
@@ -0,0 +1,8 @@
+﻿namespace CalendarBlazorApp.Data
+{
+    public class WeekClass
+    {
+        public List<DayEvent> Dates { get; set; } = new List<DayEvent>();
+    }
+}
+    
